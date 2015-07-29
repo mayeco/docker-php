a208af85d1f5200840a77edb02930694b7e52b52
Mario Young <maye.co@gmail.com>
2015-07-29 14:18:45 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,17 +4,18 @@ if [ "${PHP_INI_ENV}" != "**None**" ]; then
 
     echo "=> php.ini settings"
 
-    echo "" > /etc/php5/fpm/conf.d/zzz-php.ini
-    echo ";PHP_INI_ENV settings" >> /etc/php5/fpm/conf.d/zzz-php.ini
+    echo "" > /etc/php5/mods-available/zzz-php.ini
+    echo ";PHP_INI_ENV settings" >> /etc/php5/mods-available/zzz-php.ini
     arr=$(echo ${PHP_INI_ENV} | tr ";" "\n")
     for x in $arr
     do
         echo "=> added php.ini line: $x"
-        echo "$x" >> /etc/php5/fpm/conf.d/zzz-php.ini
+        echo "$x" >> /etc/php5/mods-available/zzz-php.ini
     done
-    echo "" > /etc/php5/fpm/conf.d/zzz-php.ini
+    echo "" > /etc/php5/mods-available/zzz-php.ini
     
-    cp -f /etc/php5/fpm/conf.d/zzz-php.ini /etc/php5/cli/conf.d/zzz-php.ini
+    ln -s /etc/php5/mods-available/zzz-php.ini /etc/php5/cli/conf.d/zzz-php.ini
+    ln -s /etc/php5/mods-available/zzz-php.ini /etc/php5/fpm/conf.d/zzz-php.ini
 
 fi
 
