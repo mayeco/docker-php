e8892b28e5117bdb7bdc8a35103b6f03643ce303
Mario Young <maye.co@gmail.com>
2015-07-29 14:17:14 -0300

    add settings in config ini

--- run.sh
+++ run.sh
@@ -4,14 +4,17 @@ if [ "${PHP_INI_ENV}" != "**None**" ]; then
 
     echo "=> php.ini settings"
 
-    echo "" >> /etc/php5/fpm/php.ini
-    echo ";PHP_INI_ENV settings" >> /etc/php5/fpm/php.ini
+    echo "" > /etc/php5/fpm/conf.d/zzz-php.ini
+    echo ";PHP_INI_ENV settings" >> /etc/php5/fpm/conf.d/zzz-php.ini
     arr=$(echo ${PHP_INI_ENV} | tr ";" "\n")
     for x in $arr
     do
         echo "=> added php.ini line: $x"
-        echo "$x" >> /etc/php5/fpm/php.ini
+        echo "$x" >> /etc/php5/fpm/conf.d/zzz-php.ini
     done
+    echo "" > /etc/php5/fpm/conf.d/zzz-php.ini
+    
+    cp -f /etc/php5/fpm/conf.d/zzz-php.ini /etc/php5/cli/conf.d/zzz-php.ini
 
 fi
 
